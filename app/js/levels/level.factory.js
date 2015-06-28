473a85825f3317c1d63c5c05c70798239770bfa8
Nick Leach <nickleach22@gmail.com>
2015-06-27 21:00:38 -0400

    FUCK YEA

--- app/js/levels/level.factory.js
+++ app/js/levels/level.factory.js
@@ -6,12 +6,36 @@
 
   .factory('LevelFactory', [function () {
 
+    var Level = function(obj){
+      this.title = obj.title;
+    };
+
     var blockFunction = function () {
 
-      var grid = $('.grid'),
+    var grid = $('.grid'),
           gTop = grid.offset().top,
           gLeft = grid.offset().left;
           cloned = $('.cloned');
+
+
+    var offsetCollection = [];
+    cloned.each(function() {
+         offsetCollection.push(
+           $(this).offset()
+         );
+    });
+    var coordinates = [];
+
+    offsetCollection.forEach( function(obj) {
+      var yy = (obj.top - gTop)/25 >> 0;
+      var xx = (obj.left - gLeft)/25 >> 0;
+      coordinates.push({x: xx, y: yy})
+
+    });
+
+
+
+
     };
 
     return {
